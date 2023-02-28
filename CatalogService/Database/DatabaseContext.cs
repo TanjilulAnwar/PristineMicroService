ad80e975fa3c2c24117a5b707a30c29a3c6f0f86
tanjilulanwar <tanjilulanwar@gmail.com>
2023-02-28 19:12:17 +0600

    Initialized DbContext

---
+++ CatalogService/Database/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogService.Database
+{
+    public class DatabaseContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+
+}
