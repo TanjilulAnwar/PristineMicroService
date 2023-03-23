d89f7e871b1552a8ca748626615143f65e4fedd5
tanjilulanwar <tanjilulanwar@gmail.com>
2023-03-23 15:41:06 +0600

    Mediatro initialized

---
+++ CatalogService/Application/Queries/GetAllProductQuery.cs
@@ -0,0 +1,10 @@
+﻿using CatalogService.Models;
+using MediatR;
+using System.Collections.Generic;
+
+namespace CatalogService.Application.Queries
+{
+    public class GetAllProductQuery : IRequest<IEnumerable<ProductModel>>
+    {
+    }
+}
